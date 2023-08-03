a70ba0bbaaf5c882e4eafb2495f39e48b3a1a039
choewy <choewy32@gmail.com>
2023-08-03 21:18:31 +0900

    feature/electron script

--- public/electron.js
+++ public/electron.js
@@ -1,32 +1,29 @@
+const ffmpeg = require('fluent-ffmpeg');
+
+ffmpeg.setFfmpegPath(require('ffmpeg-static-electron').path);
+ffmpeg.setFfprobePath(require('ffprobe-static-electron').path);
+
 const { app, BrowserWindow } = require('electron');
 
 const path = require('path');
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegStatic = require('ffmpeg-static-electron');
-const ffprobStatic = require('ffprobe-static-electron');
 
-const dev = require('electron-is-dev');
-
-ffmpeg.setFfmpegPath(ffmpegStatic.path);
-ffmpeg.setFfprobePath(ffprobStatic.path);
-
-let window = null;
+const devTools = require('electron-is-dev');
 
 const createWindow = () => {
-  window = new BrowserWindow({
+  const window = new BrowserWindow({
     width: 900,
     height: 680,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
       contextIsolation: false,
-      devTools: dev,
+      devTools,
     },
   });
 
-  window.loadURL(dev ? 'http://localhost:8000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  window.loadURL(devTools ? 'http://localhost:8000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
-  if (dev) {
+  if (devTools) {
     window.webContents.openDevTools({ mode: 'detach' });
   }
 
@@ -37,8 +34,3 @@ const createWindow = () => {
 
 app.on('ready', createWindow);
 app.on('window-all-closed', app.quit);
-app.on('ready', () => {
-  if (window === null) {
-    createWindow();
-  }
-});
