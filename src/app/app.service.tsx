f39c5222d393f256b18f4066648746518c6f0e42
choewy <choewy32@gmail.com>
2023-08-05 01:01:05 +0900

    feature/run or stop transcode

--- src/app/app.service.tsx
+++ src/app/app.service.tsx
@@ -1,12 +1,14 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
-import { FfmpegFile, FfmpegService, ffmpegService, module } from '@/core';
+import { FfmpegFile, FfmpegService, FfmpegStatus, FfmpegWorker, Logger, ffmpegService, module } from '@/core';
 import { FileListStore, FileListStoreValue, WorkerStore, fileListStore, workerStore } from '@/store';
 
 export class AppService {
   public static of() {
     return new AppService();
   }
+
+  private readonly logger = Logger.of(AppService.name);
 
   private readonly childProcess: typeof import('child_process');
   private readonly ffmpegService: FfmpegService;
@@ -18,6 +20,10 @@ export class AppService {
     this.ffmpegService = ffmpegService;
     this.workerStore = workerStore;
     this.fileListStore = fileListStore;
+  }
+
+  public get running() {
+    return this.workerStore.useValue().running;
   }
 
   public get workers() {
@@ -71,98 +77,129 @@ export class AppService {
     );
   }
 
-  public useOnResetSelectFiles() {
+  public useOnResetFilesHandler(property: keyof FileListStoreValue) {
     const setFileList = this.fileListStore.useSetState();
 
     return useCallback(
       () =>
         setFileList((prev) => ({
           ...prev,
-          selectFiles: [],
+          [property]: [],
         })),
       [setFileList],
     );
   }
 
-  public useOnResetTranscodingFiles() {
-    const setFileList = this.fileListStore.useSetState();
+  public useOnDequeue() {
+    const [{ transcodingFiles }, setFiles] = this.fileListStore.useState();
+    const [{ running, workers }, setWorkers] = this.workerStore.useState();
 
-    return useCallback(
-      () =>
-        setFileList((prev) => ({
-          ...prev,
-          transcodingFiles: [],
-        })),
-      [setFileList],
-    );
+    const file = transcodingFiles.find((f) => [FfmpegStatus.WAITING, FfmpegStatus.PAUSED].includes(f.status));
+    const worker = workers.find((w) => !w.disabled && [FfmpegStatus.WAITING, FfmpegStatus.PAUSED].includes(w.status));
+
+    useEffect(() => {
+      if (!running || !file || !worker) {
+        return setWorkers((prev) => ({ ...prev }));
+      }
+
+      this.logger.debug({
+        file: `${file?.name} : ${file?.status}`,
+        worker: `${worker?.key} : ${worker?.status}`,
+      });
+
+      setFiles((prev) => ({
+        ...prev,
+        transcodingFiles: prev.transcodingFiles.map((f) =>
+          f.key === file.key ? file.setStatus(FfmpegStatus.RUNNING) : f,
+        ),
+      }));
+
+      setWorkers((prev) => ({
+        ...prev,
+        workers: prev.workers.map((w) =>
+          w.key !== worker.key ? w : w.setStatus(FfmpegStatus.RUNNING).setFile(file).run(setWorkers, setFiles),
+        ),
+      }));
+    }, [running, file, worker, setWorkers]);
   }
 
-  public useOnStartTranscodeFiles() {
-    const setFileList = this.fileListStore.useSetState();
-
-    const onProgress = useCallback(
-      (key: string) => (value: number) => {
-        setFileList((prev) => {
-          let target = prev.transcodingFiles.find((file) => file.key === key);
-
-          if (!target) {
-            return prev;
-          }
-
-          target = Object.assign({}, target);
-          target.progress = value;
-
-          if (value < 100) {
-            return {
-              ...prev,
-              transcodingFiles: prev.transcodingFiles.map((file) => (file.key === key ? target : file)),
-            };
-          } else {
-            return {
-              ...prev,
-              transcodingFiles: prev.transcodingFiles.filter((file) => file.key !== key),
-              completeFiles: [...prev.completeFiles].concat(target),
-            };
-          }
-        });
-      },
-      [setFileList],
-    );
-
-    const onError = useCallback(
-      (key: string) => (e: any) => {
-        setFileList((prev) => {
-          let target = prev.transcodingFiles.find((file) => file.key === key);
-
-          if (!target) {
-            return prev;
-          }
-
-          target = Object.assign({}, target);
-
-          target.progress = 100;
-          target.error = e;
-          target.hasError = !!e;
-
-          return {
-            ...prev,
-            transcodingFiles: prev.transcodingFiles.filter((file) => file.key !== key),
-            completeFiles: [...prev.completeFiles].concat(target),
-          };
-        });
-      },
-      [setFileList],
-    );
+  public useOnTranscodeStart() {
+    const setFiles = this.fileListStore.useSetState();
+    const setWorkers = this.workerStore.useSetState();
 
     return useCallback(() => {
-      setFileList((prev) => ({
+      setFiles((prev) => ({
         ...prev,
-        selectFiles: prev.selectFiles.filter((file) => !!file.hasError),
-        transcodingFiles: prev.selectFiles
-          .filter((file) => !file.hasError)
-          .map((file) => this.ffmpegService.transcode(file.dupliate(), onProgress(file.key), onError(file.key))),
+        selectFiles: prev.selectFiles.filter((f) => f.hasError),
+        transcodingFiles: prev.selectFiles.filter((f) => !f.hasError),
       }));
-    }, [setFileList]);
+
+      setWorkers((prev) => ({ ...prev, running: true }));
+    }, [setFiles, setWorkers]);
+  }
+
+  public useOnTranscodeStop() {
+    const setFiles = this.fileListStore.useSetState();
+    const [{ workers }, setWorkers] = this.workerStore.useState();
+
+    return useCallback(() => {
+      const hasFileWorkers = workers.filter((w) => !!w.file);
+
+      if (hasFileWorkers.length === 0) {
+        return;
+      }
+
+      setWorkers((prev) => ({
+        ...prev,
+        running: false,
+        workers: prev.workers.map((worker) => worker.stop()),
+      }));
+
+      setFiles((prev) => {
+        const transcodingFiles = [...prev.transcodingFiles];
+
+        for (const worker of hasFileWorkers) {
+          if (!worker.file) {
+            continue;
+          }
+
+          const index = transcodingFiles.findIndex((f) => f.key === worker.file.key);
+
+          if (index > -1) {
+            transcodingFiles[index] = transcodingFiles[index].setStatus(FfmpegStatus.PAUSED);
+          } else {
+            transcodingFiles.push(worker.file.setStatus(FfmpegStatus.PAUSED));
+          }
+        }
+
+        return { ...prev, transcodingFiles };
+      });
+    }, [workers, setFiles, setWorkers]);
+  }
+
+  public useOnTranscodeRestart() {
+    const setWorkers = this.workerStore.useSetState();
+
+    return useCallback(() => {
+      setWorkers((prev) => ({
+        ...prev,
+        running: true,
+      }));
+    }, [setWorkers]);
+  }
+
+  public useOnChangeWorkerActive() {
+    const setWorkers = this.workerStore.useSetState();
+
+    return useCallback(
+      (worker: FfmpegWorker) => () => {
+        setWorkers((prev) => ({
+          ...prev,
+          workers: prev.workers.map((w) => (w.key === worker.key ? w.setDisable(!w.disabled) : w)),
+        }));
+      },
+      [setWorkers],
+    );
   }
 
   public useOnOpenDirectory() {
