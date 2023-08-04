8133f2c445bd0c60085b19d8a3f2c9085d74e633
choewy <choewy32@gmail.com>
2023-08-04 14:09:07 +0900

    feature/worker store

--- src/store/store.value.ts
+++ src/store/store.value.ts
@@ -1,4 +1,4 @@
-import { FfmpegFile } from '@/core';
+import { FfmpegFile, FfmpegWorker, module } from '@/core';
 
 export class FileListStoreValue {
   selectFiles: FfmpegFile[];
@@ -13,5 +13,29 @@ export class FileListStoreValue {
     this.selectFiles = [];
     this.transcodingFiles = [];
     this.completeFiles = [];
+  }
+}
+
+export class WorkerStoreValue {
+  running: boolean;
+  workers: FfmpegWorker[];
+
+  public static of() {
+    return new WorkerStoreValue();
+  }
+
+  constructor() {
+    const os = module.getOs();
+    const cpuCount = os.cpus().length;
+    const workerCount = cpuCount > 1 ? cpuCount - 1 : 1;
+    const activeRange = workerCount === 1 ? 1 : Math.floor(workerCount / 2);
+
+    this.running = false;
+
+    this.workers = [];
+
+    for (let i = 1; i <= workerCount; i++) {
+      this.workers.push(FfmpegWorker.of(activeRange, i));
+    }
   }
 }
