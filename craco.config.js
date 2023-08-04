2ebf9ba6dc8a8763c222de399b590d84b1b01798
choewy <choewy32@gmail.com>
2023-08-05 01:02:28 +0900

    feature/build config

--- craco.config.js
+++ craco.config.js
@@ -3,5 +3,9 @@ var path = require('path');
 module.exports = {
   webpack: {
     alias: { '@': path.resolve(__dirname, 'src') },
+    externals: {
+      'ffmpeg-static-electron': 'commonjs2 ffmpeg-static-electron',
+      'ffprobe-static-electron': 'commonjs2 ffprobe-static-electron',
+    },
   },
 };
