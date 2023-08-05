38fec3d610de24a74fc69a3579e91c2a2b807933
choewy <choewy32@gmail.com>
2023-08-05 15:13:13 +0900

    feature/transcode worker and files store property

--- src/store/store.value.ts
+++ src/store/store.value.ts
@@ -1,9 +1,15 @@
-import { FfmpegFileStoreImpl, FfmpegFile, FfmpegWorker, FfmpegWorkerStoreImpl, module } from '@/core';
+import {
+  TranscodeFileStoreImpl,
+  TranscodeFile,
+  TranscodeWorker,
+  TranscodeWorkerStoreImpl,
+  transcodeStorage,
+} from '@/core';
 
-export class FileListStoreValue implements FfmpegFileStoreImpl {
-  selectFiles: FfmpegFile[];
-  transcodingFiles: FfmpegFile[];
-  completeFiles: FfmpegFile[];
+export class FileListStoreValue implements TranscodeFileStoreImpl {
+  selectFiles: TranscodeFile[];
+  transcodingFiles: TranscodeFile[];
+  completeFiles: TranscodeFile[];
 
   public static of() {
     return new FileListStoreValue();
@@ -16,25 +22,20 @@ export class FileListStoreValue implements FfmpegFileStoreImpl {
   }
 }
 
-export class WorkerStoreValue implements FfmpegWorkerStoreImpl {
-  running: boolean;
-  workers: FfmpegWorker[];
+export class WorkerStoreValue implements TranscodeWorkerStoreImpl {
+  running: boolean = false;
+  workers: TranscodeWorker[] = [];
 
   public static of() {
     return new WorkerStoreValue();
   }
 
   constructor() {
-    const os = module.getOs();
-    const cpuCount = os.cpus().length;
-    const workerCount = cpuCount > 1 ? cpuCount - 1 : 1;
-    const activeRange = workerCount === 1 ? 1 : Math.floor(workerCount / 2);
+    const keys = transcodeStorage.getKeys();
+    const activeLimit = transcodeStorage.getDefaultLimit();
 
-    this.running = false;
-    this.workers = [];
-
-    for (let i = 1; i <= workerCount; i++) {
-      this.workers.push(FfmpegWorker.of(activeRange, i));
+    for (let i = 0; i < keys.length; i++) {
+      this.workers.push(TranscodeWorker.of(keys[i], i + 1, activeLimit));
     }
   }
 }
